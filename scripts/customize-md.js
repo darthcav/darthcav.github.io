55955f818d13c2ef2828492b1c2d3a310c7a4d6a
darthcav <darth.cav@gmail.com>
2015-11-22 15:58:01 +0100

    add custom script

---
+++ scripts/customize-md.js
@@ -0,0 +1,7 @@
+"use strict";
+
+(function ()
+{
+    var nl = document.querySelectorAll("blockquote");
+    for (var i = 0; i < nl.length; ++i) { nl[i].setAttribute("class", "blockquote"); }
+})();
